cee619172f75113d6258ced12c65f3b3c47f234f
brian <brianw0924@gmail.com>
2022-04-20 19:12:52 +0800

    init

---
+++ hw3/hw3.c
@@ -0,0 +1,80 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/mman.h>
+
+struct expose_pte_args {
+	//PID of the target task to expose pte (can be the caller task or others)
+        pid_t pid;
+	//begin userspace VA of the flattened page table
+        unsigned long begin_fpt_vaddr;
+	//end userspace VA of the flattened page table
+        unsigned long end_fpt_vaddr;
+	//begin userspace VA of the remapped PTE table
+        unsigned long begin_pte_vaddr;
+	//end userspace VA of the remapped PTE table
+        unsigned long end_pte_vaddr;
+	//begin of userspace VA to expose PTE mappings
+        unsigned long begin_vaddr;
+	//end of userspace VA to expose PTE mappings
+        unsigned long end_vaddr;
+};
+
+extern void shellcode();
+__asm__(".global shellcode\n"
+	"shellcode:\n\t"
+	/* push b'/bin///sh\x00' */
+	/* Set x14 = 8299904519029482031 = 0x732f2f2f6e69622f */
+	"mov  x14, #25135\n\t"
+	"movk x14, #28265, lsl #16\n\t"
+	"movk x14, #12079, lsl #0x20\n\t"
+	"movk x14, #29487, lsl #0x30\n\t"
+	"mov  x15, #104\n\t"
+	"stp x14, x15, [sp, #-16]!\n\t"
+	/* execve(path='sp', argv=0, envp=0) */
+	"mov  x0, sp\n\t"
+	"mov  x1, xzr\n\t"
+	"mov  x2, xzr\n\t"
+	/* call execve() */
+	"mov  x8, #221\n\t" // SYS_execve
+	"svc 0");
+
+char *create_shellcode(unsigned long len) {
+	int i;
+	char *shellcode_addr;
+
+	// allocate memory page
+	shellcode_addr = (char*)mmap(NULL, len,
+			PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
+
+	if (!shellcode_addr) {
+		fprintf(stderr, "error: %s\n", strerror(errno));
+		exit(-1);
+	};
+
+	// fill memory with nop instructions
+	for (i = 0; i < len/4; i++) {
+		((int*)shellcode_addr)[i] = 0xd503201f;  // nop opcode
+	};
+
+	// copy shellcode to memory page
+	// TODO: replace |0x100| with your shellcode length
+	memcpy(shellcode_addr + len - 0x100, &shellcode, 0x100);
+
+	return shellcode_addr;
+}
+
+int main(int argc, char* argv[])
+{
+	int ret = 0, len;
+	char *sc_begin;
+
+//	len = ...
+
+	sc_begin = create_shellcode(len); 
+//	(*(void(*)())sc_begin)();
+
+	while (1) {}
+	return ret;
+}
