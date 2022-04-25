e2bed8bca7773a823bd395bdcc4c48cc511823e4
brian <brianw0924@gmail.com>
2022-04-25 18:23:46 +0800

    Merge code

--- hw3/hw3-test.c
+++ hw3/hw3-test.c
@@ -1,10 +1,11 @@
+#include <unistd.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <errno.h>
 #include <sys/mman.h>
-#include <sys/syscall.h>
-#include <linux/types.h>
 
 struct expose_pte_args {
 	//PID of the target task to expose pte (can be the caller task or others)
@@ -28,27 +29,33 @@ struct expose_pte_args {
 #define PTRS_PER_PTE 512
 #define PTRS_PER_PMD 512
 #define PAGE_SIZE 4096
+#define PHYS_ADDR_SHIFT 32
 
 struct expose_pte_args expose_pte(pid_t pid, unsigned long begin_vaddr, unsigned long end_vaddr) {
-        unsigned long begin_fpt_vaddr, end_fpt_vaddr, begin_pte_vaddr, end_pte_vaddr;
-        unsigned long pte_cnt, pte_off, page_off;
+        unsigned long begin_fpt_vaddr, end_fpt_vaddr, begin_pte_vaddr, end_pte_vaddr, pte_cnt;
 
-        pte_cnt = 1 + (end_vaddr >> PMD_SHIFT) - (begin_vaddr >> PMD_SHIFT);
+        // Count the pmd index change, which is the number of PTE within [begin_vaddr, end_vaddr)
+        pte_cnt = 1 + ((end_vaddr - 1) >> PMD_SHIFT) - (begin_vaddr >> PMD_SHIFT);
+
+        // mmap flattened table. Each entry is 8 byte
         begin_fpt_vaddr = (unsigned long)mmap(NULL, (pte_cnt) * 8,
 			PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-        if (!begin_fpt_vaddr) {
+        if (begin_fpt_vaddr == ((unsigned long)-1)) {
 		fprintf(stderr, "error: %s\n", strerror(errno));
 		exit(-1);
 	};
         end_fpt_vaddr = begin_fpt_vaddr + pte_cnt * 8;
+
+        // mmap remapped PTE
         begin_pte_vaddr = (unsigned long)mmap(NULL, pte_cnt * PAGE_SIZE,
 			PROT_READ | PROT_WRITE | PROT_EXEC, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
-        if (!begin_pte_vaddr) {
+        if (begin_pte_vaddr == ((unsigned long)-1)) {
 		fprintf(stderr, "error: %s\n", strerror(errno));
 		exit(-1);
 	};
         end_pte_vaddr = begin_pte_vaddr + pte_cnt * PAGE_SIZE;
 
+        // system call
         struct expose_pte_args args = {
 		pid,
 		begin_fpt_vaddr,
@@ -60,7 +67,11 @@ struct expose_pte_args expose_pte(pid_t pid, unsigned long begin_vaddr, unsigned
 	};
 
         int ret = syscall(436, &args);
-        return args;
+        if(ret) {
+                printf("error: System call failed\n");
+                exit(-1);
+        } else
+                return args;
 }
 
 unsigned long *get_pte_p(struct expose_pte_args args, unsigned long va) {
@@ -71,35 +82,32 @@ unsigned long *get_pte_p(struct expose_pte_args args, unsigned long va) {
 		pte_p = (unsigned long*)pte_addr;
 		pte_offset = ((va) >> PAGE_SHIFT) & (PTRS_PER_PTE - 1);
 	        return pte_p + pte_offset;
-        } else {
-		printf("va %lx pa not exists.\n", va);
-                return 0;
-	}
+        } else
+                return NULL;
 }
 
-void code_injection(pid_t sc_pid, unsigned long sc_begin, unsigned long *target_pte_p) {
+int code_injection(pid_t sc_pid, unsigned long sc_begin, unsigned long *target_pte_p) {
         struct expose_pte_args args = expose_pte(sc_pid, sc_begin, sc_begin + PAGE_SIZE);
         unsigned long *pte_p = get_pte_p(args, sc_begin);
-        if(pte_p) {
+        if(pte_p)
                 *target_pte_p = *(pte_p);
-        }
-        else {
-                // pte doesn't exist
-        }
+        else
+                printf("error: pte not exists.\n");
+        return 0;
 }
 
-void inspection(pid_t pid, unsigned long begin_vaddr, unsigned long end_vaddr) {
+int inspection(pid_t pid, unsigned long begin_vaddr, unsigned long end_vaddr) {
         struct expose_pte_args args = expose_pte(pid, begin_vaddr, end_vaddr);
-        unsigned long va, idx, *pte_p;
+        unsigned long va, pa, idx, *pte_p;
         for(idx = 1, va = args.begin_vaddr; va < args.end_vaddr; va+=PAGE_SIZE, idx++) {
                 pte_p = get_pte_p(args, va);
                 if(pte_p) {
-                        printf("va%ld %lx pa%ld %lx\n", idx, va, idx, *pte_p);
-                } else {
+                        pa = (*pte_p) & (((unsigned long)1 << PHYS_ADDR_SHIFT) -1);
+                        printf("va%ld %lx pa%ld %lx\n", idx, va, idx, pa);
+                } else
                         printf("va%ld %lx pa%ld not exists.\n", idx, va, idx);
-                }
         }
-
+        return 0;
 }
 
 unsigned long *get_target_pte_p(pid_t tar_pid, unsigned long tar_begin, unsigned long tar_end) {
@@ -108,19 +116,32 @@ unsigned long *get_target_pte_p(pid_t tar_pid, unsigned long tar_begin, unsigned
 }
 
 int main(int argc, char *argv[]) {
-        if (argc < 5) {
-                printf("Usage: %s <tar_pid> <tar_begin_va> <tar_end_va> <sc_pid> <sc_begin>\n", argv[0]);
+        /*
+         * Error handling requirements:
+         * 0. Input addresses should aligned to page size (4K)
+         * 1. Ensure kernel does not free target task's resources
+         * 2. given task's PID should exists
+         * 3. flattened page table and remapped PTE tables should be reserved in caller's task's address space
+        */
+
+        // check arguments
+        if (argc != 4 && argc != 6) {
+                printf("Virtual Address Space Inspection: %s <tar_pid> <tar_begin_va> <tar_end_va> <sc_pid> <sc_begin>\n", argv[0]);
+                printf("Code injection: %s <tar_pid> <tar_begin_va> <tar_end_va>\n", argv[0]);
                 return 0;
         }
-        // va inspection
-        printf("va inspection\n");
-        inspection((pid_t)atoi(argv[1]), strtoul(argv[2], NULL, 16), strtoul(argv[3], NULL, 16));
 
-        // code injection
-        printf("code injection\n");
-        unsigned long *target_pte_p = get_target_pte_p((pid_t)atoi(argv[1]), strtoul(argv[2], NULL, 16), strtoul(argv[3], NULL, 16));
-        code_injection((pid_t)atoi(argv[4]), strtoul(argv[5], NULL, 16), target_pte_p);
+        int ret;
 
-        return 0;
+
+        if (argc == 4) {
+                // va inspection
+                ret = inspection((pid_t)atoi(argv[1]), strtoul(argv[2], NULL, 16), strtoul(argv[3], NULL, 16));
+        } else if (argc == 6){
+                // code injection
+                unsigned long *target_pte_p = get_target_pte_p((pid_t)atoi(argv[1]), strtoul(argv[2], NULL, 16), strtoul(argv[3], NULL, 16));
+                ret = code_injection((pid_t)atoi(argv[4]), strtoul(argv[5], NULL, 16), target_pte_p);
+        }
+        return ret;
 }
 
